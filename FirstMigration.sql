9cd59c3bd5b67dd3ba2df245fef821cebe53491c
Naght94 <linkdex456@gmail.com>
2024-03-28 06:10:07 -0400

    Added migrations and script to build the data base

---
+++ FirstMigration.sql
@@ -0,0 +1,51 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Member] (
+    [member_id] bigint NOT NULL IDENTITY,
+    [member_name] varchar(500) NOT NULL,
+    CONSTRAINT [PK_Member] PRIMARY KEY ([member_id])
+);
+GO
+
+CREATE TABLE [Product] (
+    [product_id] bigint NOT NULL IDENTITY,
+    [product_name] varchar(500) NOT NULL,
+    CONSTRAINT [PK_Product] PRIMARY KEY ([product_id])
+);
+GO
+
+CREATE TABLE [Order] (
+    [order_id] bigint NOT NULL IDENTITY,
+    [fk_memberid] bigint NOT NULL,
+    [fk_productid] bigint NOT NULL,
+    [order_dateorder] datetime2 NOT NULL,
+    CONSTRAINT [PK_Order] PRIMARY KEY ([order_id]),
+    CONSTRAINT [FK_Order_Member] FOREIGN KEY ([fk_memberid]) REFERENCES [Member] ([member_id]),
+    CONSTRAINT [FK_Order_Product] FOREIGN KEY ([fk_productid]) REFERENCES [Product] ([product_id])
+);
+GO
+
+CREATE INDEX [IX_Order_fk_memberid] ON [Order] ([fk_memberid]);
+GO
+
+CREATE INDEX [IX_Order_fk_productid] ON [Order] ([fk_productid]);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20240328100051_InitialMigration', N'8.0.3');
+GO
+
+COMMIT;
+GO
+
---
+++ Migrations/20240328100051_InitialMigration.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Trainingym.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Member",
+                columns: table => new
+                {
+                    member_id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    member_name = table.Column<string>(type: "varchar(500)", unicode: false, maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Member", x => x.member_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    product_id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    product_name = table.Column<string>(type: "varchar(500)", unicode: false, maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.product_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    order_id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    fk_memberid = table.Column<long>(type: "bigint", nullable: false),
+                    fk_productid = table.Column<long>(type: "bigint", nullable: false),
+                    order_dateorder = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.order_id);
+                    table.ForeignKey(
+                        name: "FK_Order_Member",
+                        column: x => x.fk_memberid,
+                        principalTable: "Member",
+                        principalColumn: "member_id");
+                    table.ForeignKey(
+                        name: "FK_Order_Product",
+                        column: x => x.fk_productid,
+                        principalTable: "Product",
+                        principalColumn: "product_id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_fk_memberid",
+                table: "Order",
+                column: "fk_memberid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_fk_productid",
+                table: "Order",
+                column: "fk_productid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Order");
+
+            migrationBuilder.DropTable(
+                name: "Member");
+
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
